66f6e674158dfc66d14f29334a4522892e396aa9
Eliot Alderson <e.alderson@allsafe.com>
2023-05-05 17:50:24 +0530

    Maybe Complete

--- README.md
+++ README.md
@@ -3,3 +3,22 @@
 This repo is meant to scrape Fueler Profiles and Projects/Blogs into JSON/Python dict Format, afterall whynot?
 
 Just use it as a Library. You are welcome
+
+# Sample Usage
+```
+(Fueler2JSON) ➜  fueler2json git:(master) ✗ python3 app.py -h
+usage: app.py [-h] [-u USERNAME] [-o OUTPUT] [-v]
+
+options:
+  -h, --help            show this help message and exit
+  -u USERNAME, --username USERNAME
+                        The Username of the Fueler User
+  -o OUTPUT, --output OUTPUT
+                        Output JSON file name
+  -v, --verbose         increase output verbosity
+```
+
+```python
+extract_profile(username) # Username Required
+extract_project(username, slugname) # Username and Slug name of Project Required
+```
---
+++ app.py
@@ -0,0 +1,22 @@
+from scraper import *
+import argparse
+import datetime
+import json
+
+parser = argparse.ArgumentParser()
+
+parser.add_argument('-u', '--username', type=str, help='The Username of the Fueler User', default='arnav2004')
+parser.add_argument('-o', '--output', type=str, help='Output JSON file name', default=str(datetime.datetime.now())+'.json')
+parser.add_argument('-v', '--verbose', help='increase output verbosity', action='store_true')
+
+args = parser.parse_args()
+
+print(args)
+
+p = extract_profile(args.username)
+profile = {'profile':p, 'projects':[]}
+for x in p['projects']['project']:
+    profile['projects'].append(extract_project(args.username, x['slug']))
+
+with open(args.output, 'wt') as out:
+    json.dump(profile, out)
